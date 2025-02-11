d9fb841d5b156f9641df3e16c4d841e99b0e35b5
mcarbonellmatos <mcarbonellmatos@smith.edu>
2025-02-10 20:10:36 -0500

    final

--- computer.py
+++ computer.py
@@ -1,10 +1,20 @@
 class Computer:
 
     # What attributes will it need?
+    description: str
+    processor_type: str
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
+    year_made: int
+    price: int
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, price: int, operating_system: str):
+        self.price = price
+        self.operating_system = operating_system
+    def operating_system(self,  ):
+    
 
-    # What methods will you need?
+    # What methods/functions will you need based on step 2?
